e8abdd4752c81131461f69f5009e5721189c811e
Cobalt Yang <yangfan1@pku.edu.cn>
2017-04-13 19:44:20 +0800

    make small volume higher, make quit safer

--- pypiano_main.py
+++ pypiano_main.py
@@ -195,7 +195,7 @@ class music_channel:
                                 # 如果没有在音符列表中，则直接放弃
                                 continue
                             # 用alt做弱音踏板
-                            self.volume = 0.15 if alt_pressed else 1
+                            self.volume = 0.5 if alt_pressed else 1
                             # 我们终于做好了预处理，可以创建音符了！
                             newNote = note(self, note_name, True, self.volume)
                             # 放进“正在播放”字典中
@@ -242,6 +242,8 @@ class music_channel:
         退出钢琴
         :return: 
         '''
+        if isRPi:
+            Piano_SAKS.cleanUp()
         pygame.quit()
         exit()
 
@@ -340,8 +342,8 @@ class music_mixer:
                     timing = time.time() - startTime
                     # TODO 录音状态下，将该部分写入缓冲区
                     # 添加GPIO操作，开灯关灯数字显示
-                    # if isRPi:
-                    #     threading.Thread(target=Piano_SAKS.ledOff, args=(item.tune,)).start()
+                    if isRPi:
+                        threading.Thread(target=Piano_SAKS.ledOff, args=(item.tune,)).start()
                 except Exception as e:
                     print(e)
 
