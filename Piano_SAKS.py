c7328c7d11c7d5b1922d2e44f7ebcfdf72969662
Cobalt Yang <yangfan1@pku.edu.cn>
2017-04-13 17:21:04 +0800

    add name led for piano

--- Piano_SAKS.py
+++ Piano_SAKS.py
@@ -1,23 +1,34 @@
 from sakshat import SAKSHAT
-from sakspins import SAKSPins as PINS
-import time
+# from sakspins import SAKSPins as PINS
+# import time
 
 #Declare the SAKS Board
 SAKS = SAKSHAT()
-def ledOper(string):
-	#1-7显示音调，8显示升调
-	ledNum = {'C':1,'D':2,'E':3,'F':4,'G':5,'A':6,'B':7,'+':8}
-	numOfLed = ledNum[string[-1]]
-	if numOfLed == 8:
-		sound = ledNum[string[-2]]
-		SAKS.ledrow.items[8].on()
-		SAKS.ledrow.items[sound].on()
-		time.sleep(0.5)
-		SAKS.ledrow.items[8].off()
-		SAKS.ledrow.items[sound].off()
-	else:
-		SAKS.ledrow.items[numOfLed].on()
-		time.sleep(0.5)
-		SAKS.ledrow.items[numOfLed].off()
-		#可以实现输入下一个音符时灭灯，而不是固定时间，不过我觉得
-		#如果每个音时间固定就没什么必要
+soundToNum = {'C':1,'D':2,'E':3,'F':4,'G':5,'A':6,'B':7,'+':0}
+temp = None
+def ledOn(string):
+    #1-7显示音调，0显示升调
+    global temp
+    if string[-1] == '+':
+        temp = soundToNum[string[-2]]
+        SAKS.ledrow.on_for_index(0)
+        SAKS.ledrow.on_for_index(temp)
+    else:
+        temp = soundToNum[string[-1]]
+        SAKS.ledrow.on_for_index(temp)
+
+def ledOff(string):
+    while temp != None:
+        SAKS.ledrow.off_for_index(0)
+        SAKS.ledrow.off_for_index(temp)
+
+def digitalPlay(string):
+    if len(string) == 3:
+        strToDisplay = string[0]+'#'+'.'+str(soundToNum[string[1]])+'.'+'#'
+    else:
+        strToDisplay = string[0]+'#'+str(soundToNum[string[1]])+'#'
+    SAKS.digital_display.show(strToDisplay)
+
+def cleanUp():
+    SAKS.ledrow.off()
+    SAKS.digital_display.show('####')
