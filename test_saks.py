40e221461b5cdda04c1e79b9a43d1b17f24c59ca
RibomBalt <yangfan1@pku.edu.cn>
2017-04-13 16:04:45 +0800

    fixed_saksTest

--- test_saks.py
+++ test_saks.py
@@ -3,12 +3,13 @@ import time
 saks = SAKSHAT()
 # 测试led灯
 for i in range(8):
-    saks.ledrow.on_for_status(i)
+    saks.ledrow.on_for_index(i)
     time.sleep(0.5)
-    saks.ledrow.off_for_status(i)
+    saks.ledrow.off_for_index(i)
 # 测试数码管
 for x in ('6.6.6.6', '23.33', '8888', '8#8#.','####'):
     saks.digital_display.show(x)
+    time.sleep(0.5)
     
 # 测试蜂鸣器
 saks.buzzer.beep(0.1)
